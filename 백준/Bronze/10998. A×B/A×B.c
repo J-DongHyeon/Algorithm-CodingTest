009c64185b61cf3547144a973b2766c21d853676
Jang DongHyeon <jdh3876@naver.com>
2022-08-15 20:20:05 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    
+    scanf("%d %d", &a, &b);
+    
+    while(a <= 0) scanf("%d", &a);
+    while(b >= 10) scanf("%d", &b);
+    
+    printf("%d", a*b);
+    
+    return 0;
+}
