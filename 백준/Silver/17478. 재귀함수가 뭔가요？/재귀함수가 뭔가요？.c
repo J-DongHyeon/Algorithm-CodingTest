4b696b2dc228a6868b0ca51ac467de2ace22e16e
Jang DongHyeon <jdh3876@naver.com>
2022-08-25 15:46:22 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int fun(int, int);
+
+int main(void)
+{
+    int num;
+    scanf("%d", &num);
+    puts("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+    fun(num, 0);
+}
+
+int fun(int num, int idx)
+{
+    int i;
+    for (i=0; i<idx; i++) printf("____");
+    puts("\"재귀함수가 뭔가요?\"");
+    if (num == idx)
+    {
+        for (i=0; i<idx; i++) printf("____");
+        puts("\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+    } else
+    {
+        for (i=0; i<idx; i++) printf("____");
+        puts("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+        for (i=0; i<idx; i++) printf("____");
+        puts("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+        for (i=0; i<idx; i++) printf("____");
+        puts("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+        fun(num, idx+1);
+    }
+    for (i=0; i<idx; i++) printf("____");
+    puts("라고 답변하였지.");
+}
